54744a31ea8665d175673f603fac80e7e0faf6b1
Shaedan Hawse <shaedanhawse@Shaedans-MacBook-Pro-2.local>
2024-01-07 21:37:36 -0800

    forgot to update the tab icon so that's updated, also updated the css and routing to bio

--- src/components/BioSection.js
+++ src/components/BioSection.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const BioSection = () => {
     return (
-        <section id="locations">
-            <header className="locations-head">
+        <section id="bio">
+            <header className="bio-head">
                 <h2>Empower Your Workforce with AI</h2>
-                <div className="locations-body">
+                <div className="bio-body">
                     <h3>Transform your business operations with tailored AI training for your team.</h3>
                     <p>Discover how integrating artificial intelligence and machine learning into your daily workflows can boost efficiency, enhance decision-making, and drive innovation. Our expert-led training programs are designed to equip your team with the necessary skills to harness the power of these cutting-edge technologies, custom-tailored to your industry's specific needs.</p>
                 </div>
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -9,7 +9,7 @@ const NavMenu = () => {
                 <img href="/" src={logo} alt="logo" width="5%" />
                 <h1 id="logo">Popular Consulting</h1>
                 <ul>
-                    <li><a href="#locations">About</a></li>
+                    <li><a href="#bio">About</a></li>
                     <li><a href="#services">Services</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li><a href="https://www.popularconsumption.xyz/">Blog</a></li>
