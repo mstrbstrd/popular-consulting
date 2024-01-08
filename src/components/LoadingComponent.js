816c864d158054381edb39b74bc42286ea537a7f
Shaedan Hawse <shaedanhawse@Shaedans-MacBook-Pro-2.local>
2024-01-07 21:51:45 -0800

    added a loading component to the site and VideoSection

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/LoadingComponent.css';
+
+function LoadingComponent() {
+  return (
+    <div className="loading">
+      <img src="./assets/icons/halfmoon.gif" alt="Loading..." />
+    </div>
+  );
+}
+
+export default LoadingComponent;
--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoadingComponent from './LoadingComponent';
 
 const VideoSection = ({ videoSource, sectionId, className, children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleLoadedData = () => {
+        setIsLoading(false);
+    };
+
     return (
         <section id={sectionId}>
             <div className={className}>
-                <video autoPlay muted loop id={className + "Video"} className={className + "Video"}>
+                {isLoading && <LoadingComponent />}
+                <video autoPlay muted loop id={className + "Video"} className={className + "Video"} onLoadedData={handleLoadedData}>
                     <source src={videoSource} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
