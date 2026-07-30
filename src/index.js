0f6c705aebf660dca4c26b276a06227109a29f40
mstrbstrd <104603175+mstrbstrd@users.noreply.github.com>
2026-07-30 13:39:09 -0700

    Load spectral icon colorway

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './aetheris-site.css';
 import './navigation-cohesion.css';
+import './spectral-icon-colorway.css';
 import SiteRouter from './SiteRouter';
 import InteractionAccessibilityBridge from './components/InteractionAccessibilityBridge';
 import { initCoreWebVitals, initSectionTiming, initLongTaskObserver } from './utils/telemetry';
